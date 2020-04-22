842935e72c75510ff6fb8b2dd8b99e23127c8d79
bcthis2020 <63401133+bcthis2020@users.noreply.github.com>
2020-04-22 11:18:18 +0530

    Create set-env.sh

---
+++ cicd-pyspark/set-env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This script sets the environment variables for project environment specific
+# information such as project_id, region and zone choice. And also name of
+# buckets that are used by the build pipeline and the data processing workflow.
+#
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+export TEST='test'
